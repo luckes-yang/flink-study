d98673c648c0fb0fb745c2acb074e23164b40d50
yang <xie_zhongyang@qq.com>
2022-11-25 16:46:16 +0800

    add custom sink

--- src/main/scala/org/example/stream/SingleSourceWithCheckpoint.scala
+++ src/main/scala/org/example/stream/SingleSourceWithCheckpoint.scala
@@ -1,12 +1,13 @@
 package org.example.stream
 
 import org.apache.flink.api.common.state.{ListState, ListStateDescriptor}
-import org.apache.flink.api.common.typeinfo.{IntegerTypeInfo, PrimitiveArrayTypeInfo}
+import org.apache.flink.api.common.typeinfo.PrimitiveArrayTypeInfo
 import org.apache.flink.runtime.state.{FunctionInitializationContext, FunctionSnapshotContext}
 import org.apache.flink.streaming.api.checkpoint.CheckpointedFunction
+import org.apache.flink.streaming.api.functions.sink.SinkFunction
 import org.apache.flink.streaming.api.functions.source.SourceFunction
 import org.apache.flink.streaming.api.scala.{StreamExecutionEnvironment, createTypeInformation}
-import org.slf4j.{Logger, LoggerFactory}
+import org.slf4j.LoggerFactory
 
 import java.sql.Timestamp
 import java.time.LocalDateTime
@@ -15,21 +16,22 @@ import scala.collection.JavaConverters._
 object SingleSourceWithCheckpoint {
 
   def runTask(env: StreamExecutionEnvironment): Unit = {
-    val checkpoint = "file:///opt/flink/checkpoint/SingleSourceWithCheckpoint"
-    env.getCheckpointConfig.setCheckpointStorage(checkpoint)
-    env.enableCheckpointing(60000L)
+    //    val checkpoint = "file:///opt/flink/checkpoint/SingleSourceWithCheckpoint"
+    //    env.getCheckpointConfig.setCheckpointStorage(checkpoint)
+    //    env.enableCheckpointing(60000L)
     val stream = env.addSource(new SingleSource())
-    stream.print()
+    stream.addSink(new SingleSink())
     env.execute("single source with checkpoint")
   }
 
 
-  class SingleSource extends SourceFunction[Event] with CheckpointedFunction{
+  class SingleSource extends SourceFunction[Event] with CheckpointedFunction {
     private val LOG = LoggerFactory.getLogger(classOf[SingleSource])
     private var running = true
-    private var offsetState:ListState[Array[Int]] = _
+    private var offsetState: ListState[Array[Int]] = _
     private val OFFSETS_STATE_NAME = "offsets-state"
-    @transient @volatile private var restoreOffsetState: Array[Int] = _
+    @transient
+    @volatile private var restoreOffsetState: Array[Int] = _
 
     override def snapshotState(context: FunctionSnapshotContext): Unit = {
       this.offsetState.clear()
@@ -40,7 +42,7 @@ object SingleSourceWithCheckpoint {
     override def initializeState(context: FunctionInitializationContext): Unit = {
       val stateStore = context.getOperatorStateStore
       this.offsetState = stateStore.getUnionListState(new ListStateDescriptor[Array[Int]](OFFSETS_STATE_NAME, PrimitiveArrayTypeInfo.INT_PRIMITIVE_ARRAY_TYPE_INFO))
-      if(context.isRestored){
+      if (context.isRestored) {
         this.offsetState.get().asScala.foreach(offset => {
           restoreOffsetState = offset
           LOG.info(s"restart from position: ${restoreOffsetState.head}")
@@ -52,7 +54,7 @@ object SingleSourceWithCheckpoint {
     }
 
     override def run(ctx: SourceFunction.SourceContext[Event]): Unit = {
-      while (running){
+      while (running) {
         val now = LocalDateTime.now()
         val event = Event(restoreOffsetState.head, Timestamp.valueOf(now))
         ctx.collect(event)
@@ -66,5 +68,11 @@ object SingleSourceWithCheckpoint {
     }
   }
 
-  case class Event(index:Int, time: Timestamp)
+  class SingleSink extends SinkFunction[Event] {
+    override def invoke(value: Event, context:SinkFunction.Context): Unit = {
+      println(value)
+    }
+  }
+
+  case class Event(index: Int, time: Timestamp)
 }
